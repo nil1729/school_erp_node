f36e9d8e58698446ead9668ab93776be22298b84
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-06-13 03:24:36 +0530

    change-password

--- routes/auths.js
+++ routes/auths.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const checkAuthentication = require('../middleware/checkAuthentication');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
 
 // router.post('/register', async (req, res)=> {
 //     const {email, name, role, username, password} = req.body;
@@ -33,8 +37,39 @@ router.post('/login', passport.authenticate('local', {
 }),(req, res) => {});
 
 
-router.get('/logout', async (req, res) => {
+router.get('/logout', checkAuthentication, async (req, res) => {
     req.logOut();
     res.redirect('/');
 });
+
+router.get('/change-password', checkAuthentication, async(req, res) => {
+    res.render('auths/change_password');
+});
+
+router.post('/change-password', checkAuthentication, async (req, res) => {
+    let {oldP, newP, newP2} = req.body;
+    let errors = [];
+    const user = req.user;
+    const isMatch = await bcrypt.compare(oldP, user.password);
+    if(!isMatch){
+        errors.push(`Current Password don't match`);
+    }
+    if(newP !== newP2){
+        errors.push('New passwords do not match.');
+    }
+    if(newP.trim() === ''){
+        errors.push('Password cannot contain spaces only');
+    }
+    if(newP.length < 6){
+        errors.push('Password should be atleast 6 characters long');
+    }
+    if(errors.length !== 0){
+        return res.render('auths/change_password', {errors});
+    }else{
+        user.password = await bcrypt.hash(newP, 10);
+        let result = await User.updateOne({_id: user._id}, user);
+        return res.render('auths/change_password', {errors: ['Password Changed Succesfully']});
+    }
+});
+
 module.exports = router;
